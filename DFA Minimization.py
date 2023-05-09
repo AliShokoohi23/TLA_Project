e4a5085a1af2c65201202aba5a7e7299206af09d
Ali <94232712+Alibest3@users.noreply.github.com>
2023-05-10 00:15:08 +0330

    functions added

--- DFA Minimization.py
+++ DFA Minimization.py
@@ -1,5 +1,94 @@
 import json
 
+def partition_states(states, is_final, transitions, input_symbols):
+    partitions = [set(), set()]
+    for state in states:
+        if is_final(state):
+            partitions[1].add(state)
+        else:
+            partitions[0].add(state)
+
+    updated = True
+    while updated:
+        updated = False
+        for partition in partitions:
+            for symbol in input_symbols:
+                blocks = {}
+                for state in partition:
+                    next_state = transitions[state][symbol]
+                    for idx, block in enumerate(partitions):
+                        if next_state in block:
+                            if idx not in blocks:
+                                blocks[idx] = set()
+                            blocks[idx].add(state)
+                            break
+
+                if len(blocks) > 1:
+                    partitions.remove(partition)
+                    partitions.extend(blocks.values())
+                    updated = True
+                    break
+
+            if updated:
+                break
+
+    return partitions
+
+
+def minimize_dfa(dfa):
+    states = set(dfa["states"])
+    input_symbols = set(dfa["input_symbols"])
+    transitions = dfa["transitions"]
+    initial_state = dfa["initial_state"]
+    final_states = set(dfa["final_states"])
+
+    is_final = lambda state: state in final_states
+    partitions = partition_states(states, is_final, transitions, input_symbols)
+
+    min_states = ["".join(sorted(partition)) for partition in partitions]
+    min_final_states_set = set()
+    min_transitions = {}
+    for min_state in min_states:
+        if min_state == initial_state:
+            min_initial_state = min_state
+        if min_state in final_states:
+            min_final_states = min_state
+            min_final_states_set.add(min_final_states)
+            
+
+        for symbol in input_symbols:
+                try:
+                    next_state = transitions[min_state][symbol]
+                except: 
+                    next_state = transitions[min_state[:2]][symbol]
+                for partition in partitions:
+                    if next_state in partition:
+                        min_next_state = "".join(sorted(partition))
+                        break
+                if min_state not in min_transitions:
+                    min_transitions[min_state] = {}
+                min_transitions[min_state][symbol] = min_next_state
+    
+    for final_state in min_final_states_set:
+        for states in partitions:
+            if final_state in states:
+                for state in states:
+                    min_final_states_set.add(state)
+            
+        
+            
+                
+    
+
+    
+    return {
+        "states": min_states,
+        "input_symbols": input_symbols,
+        "transitions": min_transitions,
+        "initial_state": min_initial_state,
+        "final_states": min_final_states
+    }
+
 input = "DFA.json"
 with open(input, "r") as f:
     dfa = json.load(f)
@@ -9,3 +98,7 @@ dfa["states"] = eval(dfa["states"])
 dfa["input_symbols"] = eval(dfa["input_symbols"])
 dfa["final_states"] = eval(dfa["final_states"])
 
+minimized_dfa = minimize_dfa(dfa)
+file_name = "Minimized DFA.json"
+with open(file_name, 'w') as output_file:
+        json.dump(minimized_dfa, output_file, indent=2)
