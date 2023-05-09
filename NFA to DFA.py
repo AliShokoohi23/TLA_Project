74440c446f1c9cf3bda9a79522367e09d3b4d237
Ali <94232712+Alibest3@users.noreply.github.com>
2023-05-10 00:11:44 +0330

    input added

--- NFA to DFA.py
+++ NFA to DFA.py
@@ -83,6 +83,12 @@ input_file = "NFA.json"
 with open(input_file, 'r') as f:
     nfa = json.load(f)
 
+nfa['states'] = eval(nfa['states'])
+nfa['input_symbols'] = eval(nfa['input_symbols'])
+nfa['final_states'] = eval(nfa['final_states'])
 
+for key, value in nfa['transitions'].items():
+    for k, v in value.items():
+        nfa['transitions'][key][k] = eval(v)
 
 dfa = nfa_to_dfa(nfa)
